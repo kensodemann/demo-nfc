e8148b3f307dc0e7c9c2757b1f3b7beccfca6a12
Ken Sodemann <ken@ionic.io>
2020-06-09 11:39:45 -0500

    add the NFC plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NFC, Ndef } from '@ionic-native/nfc/ngx';
+import { Plugins, StatusBarStyle } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private ndef: Ndef, private nfc: NFC, private platform: Platform) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    if (this.platform.is('hybrid')) {
+      const { StatusBar, SplashScreen } = Plugins;
+      SplashScreen.hide();
+      StatusBar.setStyle({ style: StatusBarStyle.Light });
+
+      const listener = this.nfc.addNdefListener(
+        () => console.log('added'),
+        (err: any) => console.log('failed:', err)
+      );
+      listener.subscribe(evt => {
+        console.log('received ndef message. the tag contains: ', evt.tag);
+        console.log('decoded tag id', this.nfc.bytesToHexString(evt.tag.id));
+        const message = this.ndef.textRecord('I say hello, hello, hello');
+        this.nfc.share([message]);
+      });
+    }
   }
 }
